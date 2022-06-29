8e5cbaae21a53bff559412f2eaab3d9bb96e9dbf
ashwini <ashwinib7293@gmail.com>
2022-06-29 09:30:54 +0530

    Merge pull request #1 from Ravinnpawar/ashwini_basic_batch
    committing new added files in examples
    New Changes

---
+++ springtasks/SpringExample.java
@@ -0,0 +1,17 @@
+package springtasks;
+
+//Features of Spring-
+//Predefined templates-
+//Loose Coupling is achieved.
+//LightWeight
+//Fast Development
+//Spring Architecture -
+//- IOC - Inversion of control
+//1st Component Spring Core Container- Core, Beans, Context, Expression Language
+//2nd COmponent- AOP, Aspects, Instrumentation
+//3rd- Data Access/Integration- JDBC, ORM, OXM, JMC,Transction.
+//     WEB- MVC, Remoting- Web, Servlet, Portlet, Struts.
+//IT makes the easy to test.
+
+public class SpringExample {
+}
